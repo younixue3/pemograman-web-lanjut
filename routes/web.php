817966dfbeece52789f11776ec2404a98245bbc9
younixue3 <bussiness.needs.rctka@gmail.com>
2022-09-16 21:31:02 +0800

    perteman pertama

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    echo 'Halo Dunia';
+});
+
+Route::get('/hii', function () {
+    echo 'Hii Dunia';
+});
